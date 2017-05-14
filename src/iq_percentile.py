393cb9d2cb767a2b2ba99f3a041f2d717632489e
Kevin <kevin.zp.chang@gmail.com>
2017-05-13 23:09:40 -0400

    cleaned up code, added check for already-read posts

--- src/iq_percentile.py
+++ src/iq_percentile.py
@@ -2,6 +2,12 @@ import praw
 import re
 from src import iq
 
+# Feel free to suggest additional regex patterns to match
+# patterns = {"iq is/of xxx", "xxx iq"}
+patterns = {"iq (of|is) [0-9]+", "[0-9]+\s?iq"}
+
+# Running list of comment's I've already replied to. Manually CTRL-A-DEL'ed periodically
+file = open("repliedto.txt", "a")
 
 
 def login():
@@ -9,72 +15,43 @@ def login():
     return reddit
 
 
-# Feel free to suggest additional regex patterns to match
-# If there are more patterns in the future I will probably switch to a list of regex matchers
-# and loop through.
-pat_1 = re.compile("iq (of|is) [0-9]+", re.IGNORECASE)
-pat_2 = re.compile("[0-9]+\s?iq", re.IGNORECASE)
-
-
-def run_bot(r):
+def run_bot(r, visited):
     subreddit = r.subreddit("iamverysmart")
-
-    num_posts = 4
+    num_posts = 9
 
     for submission in subreddit.hot(limit=num_posts):
         submission.comments.replace_more(limit=0)
         comment_list = submission.comments.list()
 
         for comment in comment_list:
-            text = str(comment.body)
-            reg = re.search(pat_1, text)
-            ex = re.search(pat_2, text)
+            # Make sure we don't comment twice
+            if comment.permalink() in visited:
+                continue
 
-            # Try matching "iq is/of xxx"
-            if reg:
-                print(reg.group(0))
-                print(text)
-                print(comment.permalink())
-                print("\n\n")
+            for pattern in patterns:
+                text = str(comment.body)
+                regex = re.search(re.compile(pattern, re.IGNORECASE), text)
+                if regex:
+                    print(regex.group(0) + "\n" + text + "\n" + comment.permalink() + "\n\n")
 
-                token = [int(s) for s in reg.group(0).split() if s.isdigit()]
-                percentile = iq.iq(token[0])
-                if percentile is 1:
-                    comment.reply("Wow! That's so smart I can't even find a percentile for it!"
-                                  "" + "\n\n ^^^code:https://github.com/kcdode/iq_percentile  "
-                                       "^^^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
-                elif percentile is 0:
-                    comment.reply("Wow! That IQ suggests a truly feeble mind!" +
-                                  "\n\n ^^^code:https://github.com/kcdode/iq_percentile  "
-                                  "^^^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
-                else:
-                    comment.reply("Wow! That IQ is in the " + str(percentile) + "th percentile of people!" +
-                                  "\n\n ^^^^^code:https://github.com/kcdode/iq_percentile "
-                                  "^^^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
+                    # Lookup percentile from extracted the integer in the matched string
+                    percentile = iq.iq([int(s) for s in regex.group(0).split() if s.isdigit()][0])
+                    if percentile is 1:
+                        comment.reply("Wow! That's so smart I can't even find a percentile for it!"
+                                      "" + "\n\n ^^^code:https://github.com/kcdode/iq_percentile  "
+                                           "^^^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
+                    elif percentile is 0:
+                        comment.reply("Wow! That IQ suggests a truly feeble mind!" +
+                                      "\n\n ^^^code:https://github.com/kcdode/iq_percentile  "
+                                      "^^^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
+                    else:
+                        comment.reply("Wow! That IQ is in the " + str(percentile) + "th percentile of people!" +
+                                      "\n\n ^^^code:https://github.com/kcdode/iq_percentile "
+                                      "^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
+                    file.write(comment.permalink())
+                    file.write("\n")
 
-            # Try matching "xxx iq"
-            if ex:
-                print(ex.group(0))
-                print(text)
-                print(comment.permalink())
-                print("\n\n")
+f = [line.rstrip() for line in open("repliedto.txt", "r")]
+#print(f)
 
-                token = [int(s) for s in ex.group(0).split() if s.isdigit()]
-                percentile = iq.iq(token[0])
-                if percentile is 1:
-                    comment.reply("Wow! That's so smart I can't even find a percentile for it!"
-                                  "" + "\n\n ^^^code:https://github.com/kcdode/iq_percentile  "
-                                       "^^^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
-                elif percentile is 0:
-                    comment.reply("Wow! That IQ suggests a truly feeble mind!" +
-                                  "\n\n ^^^code:https://github.com/kcdode/iq_percentile  "
-                                  "^^^^^I-am-still-in-testing-PM-me-if-I-fucked-up")
-                else:
-                    comment.reply("Wow! That IQ is in the " + str(percentile) + "th percentile of people!" +
-                                  "\n\n ^^^^^code:https://github.com/kcdode/iq_percentile "
-                                  "^^^^^I-am-still-in-testing-tell-me-if-I-fucked-up")
-
-
-
-
-run_bot(login())
+run_bot(login(), f)
